f5a4696ae7e9921835d2b3f8aba239f08976b08d
Alex Nemtt <shimanoee@yandex.ru>
2022-06-21 18:11:36 +0300

    Some changes

--- app/Program.cs
+++ app/Program.cs
@@ -4,7 +4,7 @@ class Inches {
     static void Main(){
         long inches ;
         long miles ;
-        miles = 93000000 ;
+        miles = 93_000_000 ;
         inches = miles * 5280 * 12 ;
 
         Console.WriteLine("Расстояние до солнца: " + inches ) ;
