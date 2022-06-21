317feb5bd49b88b002faea25b6cbf3234b022bce
Alex Nemtt <shimanoee@yandex.ru>
2022-06-21 16:51:34 +0300

    init

---
+++ app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System ;
+
+class Inches {
+    static void Main(){
+        long inches ;
+        long miles ;
+        miles = 93000000 ;
+        inches = miles * 5280 * 12 ;
+
+        Console.WriteLine("Расстояние до солнца: " + inches ) ;
+        
+    }
+}
