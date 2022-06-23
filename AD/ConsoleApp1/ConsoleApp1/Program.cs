ac857355f444a127d29f7e7c5866ebd8d92dfdc8
Alex Nemtt <shimanoee@yandex.ru>
2022-06-23 11:09:31 +0300

    Херня какая-то произошла

---
+++ AD/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show("all done!");
+            Console.Title = "App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*********************");
+            Console.WriteLine("****Hello yall*******");
+            Console.WriteLine("*********************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Новая папка/FirstSample/FirstSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*namespace FirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*********************");
+            Console.WriteLine("****Hello yall*******");
+            Console.WriteLine("*********************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ReadLine();
+        }
+    }
+    
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
